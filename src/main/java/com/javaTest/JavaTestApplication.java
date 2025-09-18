547ecee127b8a039ddaf57879c53a7265b997234
yehwang <ye.hwang@okestro.com>
2025-09-18 14:59:29 +0900

    feat: apply async return. h2 runtime db setting.

--- src/main/java/com/javaTest/JavaTestApplication.java
+++ src/main/java/com/javaTest/JavaTestApplication.java
@@ -2,8 +2,10 @@ package com.javaTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class JavaTestApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +13,3 @@ public class JavaTestApplication {
 	}
 
 }
-
--- src/main/java/com/javaTest/build/service/BuildService.java
+++ src/main/java/com/javaTest/build/service/BuildService.java
@@ -1,29 +1,28 @@
-package com.javaTest.deploy.service;
+package com.javaTest.build.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-
+import org.springframework.scheduling.annotation.Async;
 import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class DeployService {
+public class BuildService {
 
     @Async
-    public CompletableFuture<String> deploy() {
-        log.debug("========== [Deploy executed] ==========");
+    public CompletableFuture<String> build() {
+        log.debug("========== [Build executed] ==========");
 
         return CompletableFuture.supplyAsync(() -> {
             try {
-                Thread.sleep(5000);
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 throw new RuntimeException(e);
             }
-            return "Deploy ok";
+            return "Build ok";
         });
     }
 }
